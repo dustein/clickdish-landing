e1cc869ad425522b74b57e0c276d7ff857e01176
dustein <steindu@gmail.com>
2026-06-09 09:49:01 -0300

    img compartilha

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,25 @@
 import './globals.css';
 
 export const metadata = {
+  metadataBase: new URL('https://clickdish.fit'),
   title: 'ClickDish - sua melhor rotina de alimentação saudável e redes sociais turbinadas',
   description: 'Com o ClickDish suas refeições serão cada vez mais fit, seu corpo e alma mais leves, e suas redes sociais mais animadas.',
   openGraph: {
+    title: 'ClickDish - sua melhor rotina de alimentação saudável e redes sociais turbinadas',
+    description: 'Com o ClickDish suas refeições serão cada vez mais fit, seu corpo e alma mais leves, e suas redes sociais mais animadas.',
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'ClickDish - Sua dieta instagramável',
+      },
+    ],
+    url: 'https://clickdish.fit',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
     title: 'ClickDish - sua melhor rotina de alimentação saudável e redes sociais turbinadas',
     description: 'Com o ClickDish suas refeições serão cada vez mais fit, seu corpo e alma mais leves, e suas redes sociais mais animadas.',
     images: ['/og-image.png'],
