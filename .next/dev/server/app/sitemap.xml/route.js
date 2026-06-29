78290a74dca7553cf46dd93ce1738bc56f5efaa4
dustein <steindu@gmail.com>
2026-06-28 21:00:34 -0300

    chore: remove .next from git tracking and add to .gitignore

--- .next/dev/server/app/sitemap.xml/route.js
+++
@@ -1,9 +0,0 @@
-var R=require("../../chunks/[turbopack]_runtime.js")("server/app/sitemap.xml/route.js")
-R.c("server/chunks/src_app_sitemap--route-entry_1_prwns.js")
-R.c("server/chunks/node_modules_next_1ud9wu0._.js")
-R.c("server/chunks/node_modules_esprima_dist_esprima_1r2unxx.js")
-R.c("server/chunks/node_modules_1j_pqi9._.js")
-R.c("server/chunks/[root-of-the-server]__1eolj4i._.js")
-R.c("server/chunks/_next-internal_server_app_sitemap_xml_route_actions_05l5km9.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/sitemap--route-entry.js [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/sitemap--route-entry.js [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"OMw+GNcyJ4aaQbaZuvBnqz3bMdf0dsB3rffa13hw84U=\"\n}"
